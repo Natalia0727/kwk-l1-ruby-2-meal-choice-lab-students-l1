a9fcf57d42e76287e3bd850dff3ff5ce711f78c7
Natalia0727 <oicafn@gmail.com>
2018-06-05 17:45:11 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -10,23 +10,16 @@ end
 def lunch(food="grill cheese")
   puts "#{food} is the best lunch food!"
 end
+def dinner(food="sushi")
+  puts "#{food} is alwasy good for dinner!"
+end
 
 
 # Call the methods with puts and your own arguments here. Like this:
 puts snacks("Swedish fish")
-puts breakfast("pancakes")
-puts lunch("mac and cheese")
-## This returns "Any time, is the right time for Sweedish fish!"
 
-# call your methods here
-def breakfast(food="cereal")
-puts "every day is a good day for #{food}!"
-end
-
-def lunch(food="grilled cheese")
-  puts "#{food} is the best lunch food!"
-end
 # Call your methods without any arguments here
 puts snacks
 puts breakfast
 puts lunch
+puts dinner
