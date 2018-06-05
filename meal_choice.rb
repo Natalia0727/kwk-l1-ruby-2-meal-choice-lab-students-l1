229803e878e4b8806ee9f8d1e9c1c2bd27cda39b
Natalia0727 <oicafn@gmail.com>
2018-06-05 17:34:00 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -4,7 +4,7 @@ def snacks(food="Cheetos")
 end
 
 # Define breakfast, lunch and dinner methods that return the meal choice passed into them with a default to your favorite.
-def breakfast(food=cereal)
+def breakfast(food="cereal")
   puts "any day is a good day for #{food}"
 
 
